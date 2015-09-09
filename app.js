cdb11c9508fb0c8e6dfc268487d0abbcde41b6c1
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-09 14:16:57 -0400

    created the callback func

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 // Create a new http request object
 var request = new XMLHttpRequest();
+
+// Onreadeystatechange method Will run whenever a change in state occurs
+request.onreadystatechange = function() {
+	// When the readyState property returns a 4 it will run execute the given code
+	if (request.readyState === 4) {
+		// Chenges the html within the div with #info to the responseText property
+		document.getElementById('info').innerHTML = request.responeText;
+	}
+};
