489563827f97f0ce243d34c79cdd4bbe0f3fb71b
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-11 14:13:52 -0400

    moved status props to nested if statement

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var request = new XMLHttpRequest();
 // Onreadeystatechange method Will run whenever a change in state occurs
 request.onreadystatechange = function() {
 	// When the readyState property returns a 4 it will run execute the given code
-	// Also will execute if the status property is 200 (O.K.)
-	if (request.readyState === 4  && request.status === 200) {
-		// Chenges the html within the div with #info to the responseText property
-		document.getElementById('info').innerHTML = request.responeText;
+	if (request.readyState === 4) {
+		// Will execute if the status property is 200 (O.K.)
+		if (request.status === 200) {
+			// Chenges the html within the div with #info to the responseText property
+			document.getElementById('info').innerHTML = request.responeText;
+		} else {
+			// Alerts the user of the error
+			alert(request.statusText);
+		}
 	}
 };
 
