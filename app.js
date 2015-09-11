618804c5cada183bc48611f8fcafd5254f6057fc
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-11 14:05:16 -0400

    included status prop in conditional

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var request = new XMLHttpRequest();
 // Onreadeystatechange method Will run whenever a change in state occurs
 request.onreadystatechange = function() {
 	// When the readyState property returns a 4 it will run execute the given code
-	if (request.readyState === 4) {
+	// Also will execute if the status property is 200 (O.K.)
+	if (request.readyState === 4  && request.status === 200) {
 		// Chenges the html within the div with #info to the responseText property
 		document.getElementById('info').innerHTML = request.responeText;
 	}
