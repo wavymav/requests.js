e25a5609ddb6a79bc56c1148a24b0678778cede5
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-14 13:55:41 -0400

    for loop with concated template html

--- app.js
+++ app.js
@@ -8,7 +8,28 @@ request.onreadystatechange = function() {
 		// Will execute if the status property is 200 (O.K.)
 		if (request.status === 200) {
 			// Stores the parsed Json string(Now an Array Object) in players
-			var players = JSON.parse(request.responseText);
+			var players = JSON.parse(request.responseText),
+					template = '';
+			// Iterates through each item of players[]
+			for (var i = 0; i < players.length; i++) {
+				// Each []index (which is an {}) is passed in the player
+				player = players[i];
+				// Concats all the html elements needed in addition to giving each objects property val
+				template += '<div class="col-sm-6 col-md-4">' +
+					'<div class="thumbnail">' +
+						'<img src="' + player.image + '" alt="'+ player.name +'">' +
+						'<div class="caption">' +
+							'<h3>' + player.name + ' <strong><i>#' + player.number + '</i></strong></h3>' +
+							'<p><strong>Position: </strong>' + player.position + '</p>' +
+							'<p><strong>Age: </strong>' + player.age + '</p>' +
+							'<p><strong>Experince: </strong>' + player.experience + ' year(s)</p>' +
+							'<p><strong>College: </strong>' + player.college + '</p>' +
+						'</div>' +
+					'</div>' +
+				'</div>';
+			}
+			// Grabs the target element by # and stores the finished template within the terget element
+			document.getElementById('output').innerHTML = template;
 		}
 	}
 };
